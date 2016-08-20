8920efc6a25cc8822f8c3931b7b844113daabd74
grahamcrowell <graham.crowell@gmail.com>
2016-08-19 22:53:45 -0700

    Add logic to handle StandAloneProfile test types in ETL_PackageObject

--- AutoTest/Database/Procedure/dbo.uspInitPkgRegressionTest.sql
+++ AutoTest/Database/Procedure/dbo.uspInitPkgRegressionTest.sql
@@ -31,87 +31,103 @@ BEGIN
 	DECLARE @param nvarchar(max);
 
 	DECLARE 
-		@TableCount int = 0
+		@RegressionTableCount int = 0
+		,@ProfileTableCount int = 0
 		,@TestConfigID int
 		,@PreEtlSourceObjectFullName varchar(200)
 		,@PostEtlSourceObjectFullName varchar(200)
 		,@SnapShotBaseName varchar(200)
 		,@KeyColumns varchar(500)
-		,@PreEtlSnapShotCreationElapsedSeconds int
+		,@PreEtlSnapShotCreationElapsedSeconds int = 0
 		,@PreEtlSnapShotName varchar(200)
 		,@DatabaseName varchar(200)
 		,@SchemaName varchar(200)
 		,@TableName varchar(200)
 
--- setup regression test specs: copy configurations from AutoTest.dbo.TestConfig: insert into AutoTest.dbo.TestConfig
-INSERT INTO AutoTest.dbo.TestConfig (TestTypeID, PreEtlSourceObjectFullName, PostEtlSourceObjectFullName, TestDate, ObjectID, PkgExecKey)
-SELECT 
-	pgkobj.TestTypeID
-	,db.DatabaseName +'.'+obj.ObjectSchemaName+'.'+obj.ObjectPhysicalName AS PreEtlSourceObjectFullName
-	,db.DatabaseName +'.'+obj.ObjectSchemaName+'.'+obj.ObjectPhysicalName AS PostEtlSourceObjectFullName
-	,GETDATE()
-	,obj.ObjectID
-	,pkglog.PkgExecKey
-FROM DQMF.dbo.ETL_PackageObject AS pgkobj
-JOIN DQMF.dbo.MD_Object AS obj
-ON pgkobj.ObjectID = obj.ObjectID
-JOIN DQMF.dbo.MD_Database AS db
-ON obj.DatabaseId = db.DatabaseId
-JOIN DQMF.dbo.AuditPkgExecution AS pkglog
-ON pgkobj.PackageID = pkglog.PkgKey
-WHERE 1=1
-AND obj.IsActive = 1
-AND obj.IsObjectInDB = 1
-AND obj.ObjectPurpose = 'Fact'
-AND pkglog.PkgExecKey = @pPkgExecKey
+	-- setup regression test specs: copy configurations from AutoTest.dbo.TestConfig: insert into AutoTest.dbo.TestConfig
+	INSERT INTO AutoTest.dbo.TestConfig (TestTypeID, PreEtlSourceObjectFullName, PostEtlSourceObjectFullName, TestDate, ObjectID, PkgExecKey)
+	SELECT 
+		pgkobj.TestTypeID
+		,CASE 
+			-- Non-Comparison/Regression testing doesn't need a pre etl snap shot
+			WHEN tt.TestTypeDesc = 'RuntimeRegressionTest' THEN db.DatabaseName +'.'+obj.ObjectSchemaName+'.'+obj.ObjectPhysicalName 
+			ELSE NULL
+		END
+		AS PreEtlSourceObjectFullName
+		,db.DatabaseName +'.'+obj.ObjectSchemaName+'.'+obj.ObjectPhysicalName AS PostEtlSourceObjectFullName
+		,GETDATE()
+		,obj.ObjectID
+		,pkglog.PkgExecKey
+	FROM DQMF.dbo.ETL_PackageObject AS pgkobj
+	JOIN DQMF.dbo.MD_Object AS obj
+	ON pgkobj.ObjectID = obj.ObjectID
+	JOIN DQMF.dbo.MD_Database AS db
+	ON obj.DatabaseId = db.DatabaseId
+	JOIN DQMF.dbo.AuditPkgExecution AS pkglog
+	ON pgkobj.PackageID = pkglog.PkgKey
+	WHERE 1=1
+	AND obj.IsActive = 1
+	AND obj.IsObjectInDB = 1
+	AND obj.ObjectPurpose = 'Fact'
+	AND pkglog.PkgExecKey = @pPkgExecKey
 
-SET @TableCount = @@ROWCOUNT;
-SET @TestConfigID = @@IDENTITY;
+	SET @RegressionTableCount = @@ROWCOUNT;
+	,@ProfileTableCount int = 0
 
-DECLARE cur CURSOR
-FOR
-SELECT 
-	TestConfigID
-	,PreEtlSourceObjectFullName
-	,PostEtlSourceObjectFullName
-	,SnapShotBaseName
-	,PreEtlSnapShotName
-FROM AutoTest.dbo.TestConfig
-WHERE PkgExecKey = @pPkgExecKey
+	DECLARE cur CURSOR
+	FOR
+	SELECT 
+		TestConfigID
+		,PreEtlSourceObjectFullName
+		,PostEtlSourceObjectFullName
+		,SnapShotBaseName
+		,PreEtlSnapShotName
+	FROM AutoTest.dbo.TestConfig
+	WHERE PkgExecKey = @pPkgExecKey
+	AND PreEtlSourceObjectFullName IS NOT NULL
 
-OPEN cur;
-
-FETCH NEXT FROM cur INTO @TestConfigID, @PreEtlSourceObjectFullName, @PostEtlSourceObjectFullName, @SnapShotBaseName, @PreEtlSnapShotName
-
-WHILE @@FETCH_STATUS = 0
-BEGIN
-	-- SET @SnapShotBaseName = FORMATMESSAGE('TestConfigID%i',@TestConfigID);
-	-- SELECT @PreEtlSnapShotName = 'PreEtl_'+@SnapShotBaseName
-	DECLARE @PreEtlQuery nvarchar(max) = FORMATMESSAGE(' (SELECT * FROM %s) ', @PreEtlSourceObjectFullName);
-	SET @DatabaseName = PARSENAME(@PreEtlSourceObjectFullName,3)
-	SET @SchemaName = PARSENAME(@PreEtlSourceObjectFullName,2)
-	SET @TableName = PARSENAME(@PreEtlSourceObjectFullName,1)
-	EXEC AutoTest.dbo.uspGetKey @pDatabaseName = @DatabaseName, @pSchemaName = @SchemaName, @pObjectName = @TableName, @pColStr=@KeyColumns OUTPUT
-	EXEC @PreEtlSnapShotCreationElapsedSeconds = AutoTest.dbo.uspCreateQuerySnapShot @pQuery = @PreEtlQuery, @pKeyColumns = @KeyColumns, @pHashKeyColumns = @KeyColumns, @pDestTableName = @PreEtlSnapShotName
-
-	UPDATE TestConfig SET
-		PreEtlSourceObjectFullName = @PreEtlSourceObjectFullName
-		,PostEtlSourceObjectFullName = @PostEtlSourceObjectFullName
-		,PreEtlSnapShotCreationElapsedSeconds = @PreEtlSnapShotCreationElapsedSeconds
-		-- ,SnapShotBaseName = @SnapShotBaseName
-	FROM AutoTest.dbo.TestConfig tlog
-	WHERE tlog.TestConfigID = @TestConfigID
+	OPEN cur;
 
 	FETCH NEXT FROM cur INTO @TestConfigID, @PreEtlSourceObjectFullName, @PostEtlSourceObjectFullName, @SnapShotBaseName, @PreEtlSnapShotName
-	SET @TableCount = @TableCount + 1;
-END
-SET @TableCount = @TableCount - 1;
 
-CLOSE cur;
-DEALLOCATE cur;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		-- SET @SnapShotBaseName = FORMATMESSAGE('TestConfigID%i',@TestConfigID);
+		-- SELECT @PreEtlSnapShotName = 'PreEtl_'+@SnapShotBaseName
+		DECLARE @PreEtlQuery nvarchar(max) = FORMATMESSAGE(' (SELECT * FROM %s) ', @PreEtlSourceObjectFullName);
+		SET @DatabaseName = PARSENAME(@PreEtlSourceObjectFullName,3)
+		SET @SchemaName = PARSENAME(@PreEtlSourceObjectFullName,2)
+		SET @TableName = PARSENAME(@PreEtlSourceObjectFullName,1)
+		EXEC AutoTest.dbo.uspGetKey @pDatabaseName = @DatabaseName, @pSchemaName = @SchemaName, @pObjectName = @TableName, @pColStr=@KeyColumns OUTPUT
+		EXEC @PreEtlSnapShotCreationElapsedSeconds = AutoTest.dbo.uspCreateQuerySnapShot @pQuery = @PreEtlQuery, @pKeyColumns = @KeyColumns, @pHashKeyColumns = @KeyColumns, @pDestTableName = @PreEtlSnapShotName
+
+		UPDATE TestConfig SET
+			PreEtlSnapShotCreationElapsedSeconds = @PreEtlSnapShotCreationElapsedSeconds
+		FROM AutoTest.dbo.TestConfig tlog
+		WHERE tlog.TestConfigID = @TestConfigID
+
+		FETCH NEXT FROM cur INTO @TestConfigID, @PreEtlSourceObjectFullName, @PostEtlSourceObjectFullName, @SnapShotBaseName, @PreEtlSnapShotName
+		SET @RegressionTableCount = @RegressionTableCount + 1;
+		,@ProfileTableCount int = 0
+	END
+	SET @RegressionTableCount = @RegressionTableCount - 1;
+	,@ProfileTableCount int = 0
+
+	CLOSE cur;
+	DEALLOCATE cur;
+
+	UPDATE AutoTest.dbo.TestConfig
+		SET PreEtlSnapShotCreationElapsedSeconds = 0
+	FROM AutoTest.dbo.TestConfig
+	WHERE PkgExecKey = @pPkgExecKey
+	AND PreEtlSourceObjectFullName IS NULL
+
+	SET @ProfileTableCount = @@ROWCOUNT
 
 	SELECT @runtime=DATEDIFF(second, @start, sysdatetime());
-	RAISERROR('!dbo.uspInitPkgRegression: runtime: %i seconds (%i table/view is prepared for testing)', 0, 1, @runtime, @TableCount) WITH NOWAIT;
+	RAISERROR('!dbo.uspInitPkgRegression: runtime: %i seconds (%i table/view are prepared for regression testing, %i tables/views will be stand alone profiled)', 0, 1, @runtime, @RegressionTableCount, @ProfileTableCount) WITH NOWAIT;
+	,@ProfileTableCount int = 0
 	RETURN(@runtime);
+
 END
 GO
--- AutoTest/Database/Procedure/dbo.uspPkgRegressionTest.sql
+++ AutoTest/Database/Procedure/dbo.uspPkgRegressionTest.sql
@@ -29,16 +29,19 @@ BEGIN
 	DECLARE @param nvarchar(max);
 
 	DECLARE @TestConfigID int
+		,@TestTypeID int
+		,@TestTypeDesc varchar(200)
 		,@PreEtlSourceObjectFullName varchar(200)
 		,@PostEtlSourceObjectFullName varchar(200)
 		,@SnapShotBaseName varchar(200)
 		,@KeyColumns varchar(500)
 		,@PostEtlSnapShotCreationElapsedSeconds int
-		,@PostSnapShotName varchar(200)
+		-- ,@PostSnapShotName varchar(200)
 		,@ComparisonRuntimeSeconds int
 		,@DatabaseName varchar(200)
 		,@SchemaName varchar(200)
-		,@TableName varchar(200)
+		,@PreEtlTableName varchar(200)
+		,@PostEtlTableName varchar(200)
 
 -- INSERT INTO AutoTest.dbo.TestConfig (PreEtlSourceObjectFullName, PostEtlSourceObjectFullName, ObjectID, TestConfigID, PkgExecKey)
 -- SELECT 
@@ -70,36 +73,54 @@ DECLARE reg_cur CURSOR
 FOR
 SELECT 
 	TestConfigID
+	,tt.TestTypeDesc
 	,PreEtlSourceObjectFullName
 	,PostEtlSourceObjectFullName
-	,SnapShotBaseName
-FROM AutoTest.dbo.TestConfig
+	,PostEtlSnapShotName
+FROM AutoTest.dbo.TestConfig AS config
+JOIN AutoTest.dbo.TestType AS tt
+ON config.TestTypeID = tt.TestTypeID
 WHERE PkgExecKey = @pPkgExecKey
 
 OPEN reg_cur;
 
-FETCH NEXT FROM reg_cur INTO @TestConfigID, @PreEtlSourceObjectFullName, @PostEtlSourceObjectFullName, @SnapShotBaseName
+FETCH NEXT FROM reg_cur INTO @TestConfigID, @TestTypeDesc, @PreEtlSourceObjectFullName, @PostEtlSourceObjectFullName, @PostEtlSnapShotName
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-	SET @SnapShotBaseName = FORMATMESSAGE('TestConfigID%i',@TestConfigID);
-	SELECT @PostSnapShotName = 'PostEtl_'+@SnapShotBaseName
-	DECLARE @PostEtlQuery nvarchar(max) = FORMATMESSAGE(' (SELECT * FROM %s) ', @PostEtlSourceObjectFullName);
-	SET @DatabaseName = PARSENAME(@PreEtlSourceObjectFullName,3)
-	SET @SchemaName = PARSENAME(@PreEtlSourceObjectFullName,2)
-	SET @TableName = PARSENAME(@PreEtlSourceObjectFullName,1)
-	EXEC AutoTest.dbo.uspGetKey @pDatabaseName = @DatabaseName, @pSchemaName = @SchemaName, @pObjectName = @TableName, @pColStr=@KeyColumns OUTPUT
-	EXEC @PostEtlSnapShotCreationElapsedSeconds = AutoTest.dbo.uspCreateQuerySnapShot @pQuery = @PostEtlQuery, @pKeyColumns = @KeyColumns, @pHashKeyColumns = @KeyColumns, @pDestTableName = @PostSnapShotName
-
-	EXEC @ComparisonRuntimeSeconds = AutoTest.dbo.uspDataCompare @pTestConfigID = @TestConfigID
-
+	IF @TestTypeDesc IN ('RuntimeRegressionTest', 'StandAloneProfile')
+	BEGIN
+		-- SET @PostEtlSnapShotName = FORMATMESSAGE('TestConfigID%i',@TestConfigID);
+		-- SELECT @PostSnapShotName = 'PostEtl_'+@PostEtlSnapShotName
+		DECLARE @PostEtlQuery nvarchar(max) = FORMATMESSAGE(' (SELECT * FROM %s) ', @PostEtlSourceObjectFullName);
+		SET @DatabaseName = PARSENAME(@PreEtlSourceObjectFullName,3)
+		SET @SchemaName = PARSENAME(@PreEtlSourceObjectFullName,2)
+		SET @PreEtlTableName = PARSENAME(@PreEtlSourceObjectFullName,1)
+		SET @PostEtlTableName = PARSENAME(@PostEtlSourceObjectFullName,1)
+	END
+	IF @TestTypeDesc IN ('RuntimeRegressionTest', )
+	BEGIN
+		EXEC AutoTest.dbo.uspGetKey @pDatabaseName = @DatabaseName, @pSchemaName = @SchemaName, @pObjectName = @PreEtlTableName, @pColStr=@KeyColumns OUTPUT
+		EXEC @PostEtlSnapShotCreationElapsedSeconds = AutoTest.dbo.uspCreateQuerySnapShot @pQuery = @PostEtlQuery, @pKeyColumns = @KeyColumns, @pHashKeyColumns = @KeyColumns, @pDestTableName = @PostEtlTableName
+		EXEC @ComparisonRuntimeSeconds = AutoTest.dbo.uspDataCompare @pTestConfigID = @TestConfigID
+	END
+	ELSE IF @TestTypeDesc IN ('StandAloneProfile')
+	BEGIN
+		DECLARE @StandAloneTableProfileTypeID int;
+		DECLARE @StandAloneColumnProfileTypeID int;
+		DECLARE @StandAloneColumnHistogramTypeID int;
+		SELECT @StandAloneTableProfileTypeID = TableProfileTypeID FROM AutoTest.dbo.TableProfileType WHERE TableProfileTypeDesc = 'StandAloneTableProfile'
+		SELECT @StandAloneColumnProfileTypeID = ColumnProfileTypeID FROM AutoTest.dbo.ColumnProfileType WHERE ColumnProfileTypeDesc = 'StandAloneColumnProfile'
+		SELECT @StandAloneColumnHistogramTypeID = ColumnHistogramTypeID FROM AutoTest.dbo.ColumnHistogramType WHERE ColumnHistogramTypeDesc = 'StandAloneColumnHistogram'
+		EXEC @ComparisonRuntimeSeconds = AutoTest.dbo.uspCreateProfile @pTestConfigID = @pTestConfigID, @pTargetTableName = @PostEtlTableName, @pTableProfileTypeID = @StandAloneTableProfileTypeID, @pColumnProfileTypeID = @StandAloneColumnProfileTypeID, @pColumnHistogramTypeID = @StandAloneColumnHistogramTypeID;
+	END
 	UPDATE TestConfig SET
 		PostEtlSnapShotCreationElapsedSeconds = @PostEtlSnapShotCreationElapsedSeconds
 		,ComparisonRuntimeSeconds = @ComparisonRuntimeSeconds
 	FROM TestConfig tlog
 	WHERE tlog.TestConfigID = @TestConfigID
 
-	FETCH NEXT FROM reg_cur INTO @TestConfigID, @PreEtlSourceObjectFullName, @PostEtlSourceObjectFullName, @SnapShotBaseName
+	FETCH NEXT FROM reg_cur INTO @TestConfigID, @TestTypeDesc, @PreEtlSourceObjectFullName, @PostEtlSourceObjectFullName, @PostEtlSnapShotName
 END
 
 
