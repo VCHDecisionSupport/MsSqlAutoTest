58964371118d4c563fd15d230b9acc785ed5a77f
grahamcrowell <GCrowell@vch.ca>
2016-08-26 21:01:01 -0700

    FK inference

--- unittests/unittests.sql
+++ unittests/unittests.sql
@@ -223,3 +223,176 @@ END
 );',@postcommand = 'SELECT * FROM ##tab order by rcount desc; DROP TABLE ##tab;'
 END
 --#endregoin row counts
+
+
+
+IF 1=2
+BEGIN
+DECLARE @DatabaseName varchar(500) = 'CommunityMart'
+
+DECLARE @Demotions TABLE (
+	ParentTableName varchar(100)
+	,NewIDColumnName varchar(100)
+);
+
+INSERT INTO @Demotions VALUES ('dbo.ReferralFact','LocalReportingOfficeID')
+
+;WITH mr_Profile AS (
+	SELECT MAX(TestConfigID) AS TestConfigID, tlog.PreEtlSourceObjectFullName
+	FROM AutoTest.dbo.TestConfig AS tlog
+	GROUP BY tlog.PreEtlSourceObjectFullName
+), ck AS (
+	SELECT tlog.PreEtlSourceObjectFullName, cpro.ColumnName AS CandidateKey, tab.object_id, tpro.RecordCount, cpro.ColumnCount, tlog.TestConfigID
+	FROM AutoTest.dbo.TestConfig AS tlog
+	JOIN mr_Profile
+	ON mr_Profile.TestConfigID = tlog.TestConfigID
+	JOIN CommunityMart.sys.schemas AS sch
+	ON sch.name = PARSENAME(tlog.PreEtlSourceObjectFullName,2)
+	JOIN CommunityMart.sys.tables AS tab
+	ON tab.name = PARSENAME(tlog.PreEtlSourceObjectFullName,1)
+	AND tab.schema_id = sch.schema_id
+	JOIN AutoTest.dbo.TableProfile AS tpro
+	ON tlog.TestConfigID = tpro.TestConfigID
+	JOIN AutoTest.dbo.ColumnProfile AS cpro
+	ON tpro.TableProfileID = cpro.TableProfileID
+	JOIN CommunityMart.sys.columns AS col
+	ON tab.object_id = col.object_id
+	AND cpro.TableProfileID = tpro.TableProfileID
+	AND cpro.ColumnName = col.name
+	WHERE tpro.RecordCount = cpro.ColumnCount
+	AND tpro.RecordCount > 1
+	AND cpro.ColumnName NOT IN ('__idkey__', 'ETLAuditID')
+	AND cpro.ColumnName LIKE '%ID'
+),parent_child_pairs  AS (
+	SELECT child_schema.name+'.'+ child.name AS ChildTable, PARSENAME(ParentCk.PreEtlSourceObjectFullName,2)+'.'+ PARSENAME(ParentCk.PreEtlSourceObjectFullName,1) AS ParentTable, fk.name AS JoinColumn, ParentCk.RecordCount AS ParentRecordCount, ParentCk.ColumnCount AS ParentColumnCount, ChildCk.RecordCount AS ChildRecordCount, child_cpro.ColumnCount AS ChildColumnCount
+	FROM ck AS ParentCk
+	JOIN CommunityMart.sys.columns AS fk
+	ON ParentCk.CandidateKey = fk.name
+	JOIN CommunityMart.sys.tables AS child
+	ON fk.object_id = child.object_id
+
+	JOIN CommunityMart.sys.schemas AS child_schema
+	ON child.schema_id = child_schema.schema_id
+	JOIN ck AS ChildCk
+	ON PARSENAME(ChildCk.PreEtlSourceObjectFullName,2)+'.'+ PARSENAME(ChildCk.PreEtlSourceObjectFullName,1) = child_schema.name+'.'+ child.name
+	JOIN AutoTest.dbo.TestConfig AS child_tlog
+	ON child_tlog.TestConfigID = ChildCk.TestConfigID
+	JOIN AutoTest.dbo.TableProfile AS child_tpro
+	ON child_tpro.TestConfigID = child_tlog.TestConfigID
+	JOIN AutoTest.dbo.ColumnProfile AS child_cpro
+	ON child_tpro.TableProfileID = child_cpro.TableProfileID
+	AND child_cpro.ColumnName = fk.name
+	WHERE child_cpro.ColumnCount <= ParentCk.RecordCount
+	AND ChildCk.PreEtlSourceObjectFullName != ParentCk.PreEtlSourceObjectFullName
+	--JOIN ck AS child
+	--ON parent.CandidateKey = child.CandidateKey
+	--AND parent.RecordCount <= child.RecordCount
+	--AND parent.PreEtlSourceObjectFullName != child.PreEtlSourceObjectFullName
+	--JOIN CommunityMart.sys.columns AS col
+	--ON ck.ColumnName = col.name
+	--AND ck.object_id != col.object_id
+	--JOIN CommunityMart.sys.tables AS tab
+	--ON col.object_id = tab.object_id
+	--JOIN CommunityMart.sys.schemas AS sch
+	--ON tab.schema_id = sch.schema_id
+	--GROUP BY ck.PreEtlSourceObjectFullName, ck.ColumnName, sch.name, tab.name, col.name
+), roots AS (
+	SELECT parent_child_pairs.ParentTable AS RootTableName
+	FROM parent_child_pairs
+	WHERE PARSENAME(parent_child_pairs.ParentTable,2) != 'Dim'
+	EXCEPT 
+	SELECT parent_child_pairs.ChildTable AS RootTableName
+	FROM parent_child_pairs
+	WHERE PARSENAME(parent_child_pairs.ParentTable,2) != 'Dim'
+), tree AS (
+	SELECT RootTableName AS ChildTable, NULL AS ParentTable, NULL AS JoinColumn
+	FROM roots
+	UNION
+	SELECT ChildTable, ParentTable, parent_child_pairs.JoinColumn
+	FROM parent_child_pairs
+), recursive_build AS (
+	SELECT tree.ChildTable, tree.ParentTable, tree.JoinColumn, CAST('' AS varchar(max)) AS DemotedTables
+	FROM tree
+	WHERE tree.ParentTable IS NULL
+	UNION ALL
+	SELECT tree.ChildTable, tree.ParentTable, tree.JoinColumn, CAST(DemotedTables + ','+recursive_build.ChildTable AS varchar(max)) AS DemotedTables
+	FROM tree
+	JOIN recursive_build
+	ON tree.ParentTable = recursive_build.ChildTable
+)
+SELECT *
+--FROM parent_child_pairs
+--WHERE parent_child_pairs.ChildTable IN ('dbo.DischargeFact', 'dbo.ReferralFact')
+--ORDER BY parent_child_pairs.ParentTable, parent_child_pairs.ChildTable
+--OPTION (MAXRECURSION 0);
+FROM parent_child_pairs
+--WHERE (parent_child_pairs.ChildTable LIKE '%ReferralFact%'
+--OR parent_child_pairs.ChildTable LIKE '%DischargeFact%')
+--AND (parent_child_pairs.ChildTable LIKE '%Fact%'
+--AND parent_child_pairs.ParentTable LIKE '%Fact%')
+ORDER BY parent_child_pairs.ChildTable
+
+
+END
+
+
+IF 1=2
+
+BEGIN
+
+
+
+USE CommunityMart
+DECLARE @Demotions TABLE (
+	TableName varchar(100)
+	,LookupColumnName varchar(100)
+	,NewColumnName varchar(100)
+);
+
+INSERT INTO @Demotions VALUES ('dbo.PersonFact', '', '');
+
+SELECT tlog.PreEtlSourceObjectFullName, cpro.ColumnName
+FROM AutoTest.dbo.TestConfig AS tlog
+JOIN AutoTest.dbo.TestType AS tt
+ON tlog.TestTypeID = tt.TestTypeID
+JOIN AutoTest.dbo.TableProfile AS tpro
+ON tlog.TestConfigID = tpro.TestConfigID
+JOIN AutoTest.dbo.ColumnProfile AS cpro
+ON cpro.TableProfileID = tpro.TableProfileID
+WHERE tpro.RecordCount = cpro.ColumnCount
+AND cpro.ColumnName LIKE '%ID'
+AND tpro.RecordCount != 0
+
+SELECT COUNT(DISTINCT LHAID) LHAID, COUNT(DISTINCT PostalCodeID) PostalCodeID
+FROM CommunityMart.Dim.PostalCode
+
+DECLARE fact_cur CURSOR
+FOR 
+SELECT DISTINCT DB_NAME(), sch.name, tab.name 
+FROM CommunityMart.sys.tables AS tab
+JOIN CommunityMart.sys.schemas AS sch
+ON tab.schema_id = sch.schema_id
+WHERE tab.name LIKE '%Fact'
+AND DB_NAME()+'.'+ sch.name+'.'+tab.name NOT IN (
+	SELECT PreEtlSourceObjectFullName
+	FROM AutoTest.dbo.TestConfig 
+)
+
+DECLARE @DatabaseName varchar(500);
+DECLARE @SchemaName varchar(500);
+DECLARE @TableName varchar(500);
+
+OPEN fact_cur;
+
+FETCH NEXT FROM fact_cur INTO @DatabaseName, @SchemaName, @TableName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	PRINT FORMATMESSAGE('%s.%s.%s','CommunityMart', 'dbo', @TableName);
+	EXEC AutoTest.dbo.uspAdHocDataProfile 'CommunityMart', 'dbo', @TableName;
+	FETCH NEXT FROM fact_cur INTO @DatabaseName, @SchemaName, @TableName;
+END
+
+CLOSE fact_cur;
+DEALLOCATE fact_cur;
+END
