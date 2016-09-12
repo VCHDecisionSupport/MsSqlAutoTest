dea253dbc2560132a120977230715e8fc3b28bd7
grahamcrowell <graham.crowell@gmail.com>
2016-09-12 16:30:47 -0700

    init and pySqlGraph

--- Denormalizer/dbo.ReferralFact.sql
+++ Denormalizer/dbo.ReferralFact.sql
@@ -1,113 +1,59 @@
-
-
---#region BR dbo.ReferralFact
-SET @UpdatedBy = 'VCH\GCrowell'
-SET @CreatedBy ='VCH\GCrowell'
-SET @IsActive = 1;
-SET @IsLogged = 1;
-SET @GUID = '7a4a6166-77c7-11e6-b0c5-00232444dba2'
-SELECT @BRID=BRID FROM dbo.DQMF_BizRule WHERE GUID=@GUID
-
-SET @ActionSQL = '
-BEGIN
-	SELECT
-	ReferralReasonID
-	,DischargeOutcomeID
-	,ReferralTypeID
-	,SourceSystemClientID
-	,EthnicityID
-	,EducationLevelCodeID
-	,EducationLevelID
-	,DeathLocationID
-	,GenderID
-	,ReasonEndingServiceCodeID
-	,ReasonEndingServiceID
-	,DischargeDispositionCodeID
-	,DischargeDispositionID
-	,ReferralSourceLookupID
-	,ReferralSourceID
-	,LocalReportingOfficeID
-	,ProviderID
-	,LevelOfCareId
-	,FacilityID
-	,HSDAID
-	,CommunityProgramID
-	,ReferralPriorityID
+SELECT
+	Dim.LocalReportingOffice.CommunityProgramID
+	,Dim.LocalReportingOffice.ProviderID
+	,Dim.Provider.LevelOfCareId
+	,Dim.Provider.FacilityID
+	,Dim.Facility.HSDAID
+	,Dim.ReferralSourceLookup.ReferralSourceID
+	,Dim.DischargeDispositionLookupCOM.DischargeDispositionID
+	,Dim.ReasonEndingServiceLookup.ReasonEndingServiceID
+	,dbo.PersonFact.DeathLocationID
+	,dbo.PersonFact.EthnicityID
+	,dbo.PersonFact.EducationLevelCodeID
+	,Dim.EducationLevelLookup.EducationLevelID
+	,dbo.PersonFact.GenderID
 FROM dbo.ReferralFact
-JOIN Dim.ReferralReason
-ON Dim.ReferralReason.ReferralReasonID=dbo.ReferralFact.ReferralReasonID
-JOIN Dim.DischargeOutcome
-ON Dim.DischargeOutcome.DischargeOutcomeID=dbo.ReferralFact.DischargeOutcomeID
-JOIN Dim.ReferralType
-ON Dim.ReferralType.ReferralTypeID=dbo.ReferralFact.ReferralTypeID
-JOIN dbo.PersonFact
-ON dbo.PersonFact.SourceSystemClientID=dbo.ReferralFact.SourceSystemClientID
-	JOIN Dim.Ethnicity
-	ON Dim.Ethnicity.EthnicityID=dbo.PersonFact.EthnicityID
-	JOIN Dim.EducationLevelLookup
-	ON Dim.EducationLevelLookup.EducationLevelCodeID=dbo.PersonFact.EducationLevelCodeID
-		JOIN Dim.EducationLevel
-		ON Dim.EducationLevel.EducationLevelID=Dim.EducationLevelLookup.EducationLevelID
-	JOIN Dim.DeathLocation
-	ON Dim.DeathLocation.DeathLocationID=dbo.PersonFact.DeathLocationID
-	JOIN Dim.Gender
-	ON Dim.Gender.GenderID=dbo.PersonFact.GenderID
-JOIN Dim.ReasonEndingServiceLookup
-ON Dim.ReasonEndingServiceLookup.ReasonEndingServiceCodeID=dbo.ReferralFact.ReasonEndingServiceCodeID
-	JOIN Dim.ReasonEndingService
-	ON Dim.ReasonEndingService.ReasonEndingServiceID=Dim.ReasonEndingServiceLookup.ReasonEndingServiceID
-JOIN Dim.DischargeDispositionLookupCOM
-ON Dim.DischargeDispositionLookupCOM.DischargeDispositionCodeID=dbo.ReferralFact.DischargeDispositionCodeID
-	JOIN Dim.DischargeDisposition
-	ON Dim.DischargeDisposition.DischargeDispositionID=Dim.DischargeDispositionLookupCOM.DischargeDispositionID
-JOIN Dim.ReferralSourceLookup
-ON Dim.ReferralSourceLookup.ReferralSourceLookupID=dbo.ReferralFact.ReferralSourceLookupID
-	JOIN Dim.ReferralSource
-	ON Dim.ReferralSource.ReferralSourceID=Dim.ReferralSourceLookup.ReferralSourceID
-JOIN Dim.LocalReportingOffice
+LEFT JOIN Dim.LocalReportingOffice
 ON Dim.LocalReportingOffice.LocalReportingOfficeID=dbo.ReferralFact.LocalReportingOfficeID
-	JOIN Dim.Provider
-	ON Dim.Provider.ProviderID=Dim.LocalReportingOffice.ProviderID
-		JOIN Dim.LevelOfCare
-		ON Dim.LevelOfCare.LevelOfCareId=Dim.Provider.LevelOfCareId
-		JOIN Dim.Facility
-		ON Dim.Facility.FacilityID=Dim.Provider.FacilityID
-			JOIN Dim.HSDA
-			ON Dim.HSDA.HSDAID=Dim.Facility.HSDAID
-	JOIN Dim.CommunityProgram
+	LEFT JOIN Dim.CommunityProgram
 	ON Dim.CommunityProgram.CommunityProgramID=Dim.LocalReportingOffice.CommunityProgramID
-JOIN Dim.ReferralPriority
+	LEFT JOIN Dim.Provider
+	ON Dim.Provider.ProviderID=Dim.LocalReportingOffice.ProviderID
+		LEFT JOIN Dim.LevelOfCare
+		ON Dim.LevelOfCare.LevelOfCareId=Dim.Provider.LevelOfCareId
+		LEFT JOIN Dim.Facility
+		ON Dim.Facility.FacilityID=Dim.Provider.FacilityID
+			LEFT JOIN Dim.HSDA
+			ON Dim.HSDA.HSDAID=Dim.Facility.HSDAID
+LEFT JOIN Dim.ReferralSourceLookup
+ON Dim.ReferralSourceLookup.ReferralSourceLookupID=dbo.ReferralFact.ReferralSourceLookupID
+	LEFT JOIN Dim.ReferralSource
+	ON Dim.ReferralSource.ReferralSourceID=Dim.ReferralSourceLookup.ReferralSourceID
+LEFT JOIN Dim.DischargeDispositionLookupCOM
+ON Dim.DischargeDispositionLookupCOM.DischargeDispositionCodeID=dbo.ReferralFact.DischargeDispositionCodeID
+	LEFT JOIN Dim.DischargeDisposition
+	ON Dim.DischargeDisposition.DischargeDispositionID=Dim.DischargeDispositionLookupCOM.DischargeDispositionID
+LEFT JOIN Dim.ReferralPriority
 ON Dim.ReferralPriority.ReferralPriorityID=dbo.ReferralFact.ReferralPriorityID
-END
-'
-EXEC [dbo].[SetBizRule] 
-@pBRId=@BRID, 
-@pShortNameOfTest='dbo.ReferralFact', 
-@pRuleDesc='', 
-@pConditionSQL='', 
-@pActionID=, 
-@pActionSQL=@ActionSQL,
-@pOlsonTypeID=NULL, 
-@pSeverityTypeID=NULL, 
-@pSequence=, 
-@pDefaultValue='0', 
-@pDatabaseId=32, 
-@pTargetObjectPhysicalName='', 
-@pTargetAttributePhysicalName='', 
-@pSourceObjectPhysicalName=NULL, 
-@pSourceAttributePhysicalName=NULL, 
-@pIsActive=@IsActive, 
-@pComment='generated with python', 
-@pCreatedBy=@CreatedBy,
-@pUpdatedBy=@UpdatedBy,
-@pIsLogged=@IsLogged, 
-@pGUID=@GUID, 
-@pFactTableObjectAttributeId=NULL
-
-IF NOT EXISTS (SELECT * FROM dbo.DQMF_BizRuleSchedule bsc INNER JOIN dbo.DQMF_BizRule br ON bsc.BRID=br.BRID WHERE scheduleid= @DQMF_ScheduleId AND br.BRID=(SELECT BRID FROM DQMF_BizRule WHERE GUID=@GUID))
-INSERT dbo.DQMF_BizRuleSchedule (BRID, ScheduleID) SELECT (SELECT BRID FROM DQMF_BizRule WHERE GUID=@GUID) AS BRID, @DQMF_ScheduleId 
-
-DELETE BRM FROM dbo.DQMF_BizRuleLookupMapping BRM INNER JOIN dbo.DQMF_BizRule BR ON BRM.BRID = BR.BRID WHERE BR.GUID = @GUID
-UPDATE DQMF.dbo.DQMF_BizRule SET IsLogged=@IsLogged, IsActive=@IsActive FROM DQMF.dbo.DQMF_BizRule AS br WHERE br.GUID = @GUID;
---#endregion BR dbo.ReferralFact
-
+LEFT JOIN Dim.ReasonEndingServiceLookup
+ON Dim.ReasonEndingServiceLookup.ReasonEndingServiceCodeID=dbo.ReferralFact.ReasonEndingServiceCodeID
+	LEFT JOIN Dim.ReasonEndingService
+	ON Dim.ReasonEndingService.ReasonEndingServiceID=Dim.ReasonEndingServiceLookup.ReasonEndingServiceID
+LEFT JOIN Dim.ReferralReason
+ON Dim.ReferralReason.ReferralReasonID=dbo.ReferralFact.ReferralReasonID
+LEFT JOIN dbo.PersonFact
+ON dbo.PersonFact.SourceSystemClientID=dbo.ReferralFact.SourceSystemClientID
+	LEFT JOIN Dim.DeathLocation
+	ON Dim.DeathLocation.DeathLocationID=dbo.PersonFact.DeathLocationID
+	LEFT JOIN Dim.Ethnicity
+	ON Dim.Ethnicity.EthnicityID=dbo.PersonFact.EthnicityID
+	LEFT JOIN Dim.EducationLevelLookup
+	ON Dim.EducationLevelLookup.EducationLevelCodeID=dbo.PersonFact.EducationLevelCodeID
+		LEFT JOIN Dim.EducationLevel
+		ON Dim.EducationLevel.EducationLevelID=Dim.EducationLevelLookup.EducationLevelID
+	LEFT JOIN Dim.Gender
+	ON Dim.Gender.GenderID=dbo.PersonFact.GenderID
+LEFT JOIN Dim.DischargeOutcome
+ON Dim.DischargeOutcome.DischargeOutcomeID=dbo.ReferralFact.DischargeOutcomeID
+LEFT JOIN Dim.ReferralType
+ON Dim.ReferralType.ReferralTypeID=dbo.ReferralFact.ReferralTypeID
--- Denormalizer/pySqlGraph.py
+++ Denormalizer/pySqlGraph.py
@@ -7,6 +7,24 @@ from BizRuleMaker import BizRule
 # TODO: handle quoting is BR generation
 # TODO: document sql query, generate docstring
 
+# """
+# 	pySqlGraph.SqlGraph uses Networkx to generate 
+# 	python C:\Users\gcrowell\AppData\Local\Programs\Python\Python35\Lib\pydoc.py -w Z:\GITHUB\PyUtilities\Denormalizer\pySqlGraph.py
+# """
+
+def get_alias(table_name):
+	# vowels = 'aoeui'
+	suffix = '_fact'
+	if table_name[-4:len(table_name)] == 'Fact':
+		table_name = table_name[0:-4]
+	else:
+		suffix = '_dim'
+
+	# alias = table_name.split('.')[1].lower()[0:4]+(''.join([l for l in (table_name.split('.')[1].lower()[4:len(table_name)]) if l not in vowels]))[0] + suffix
+	alias = table_name.split('.')[1].lower() + suffix
+	# print('{} alias {}'.format(table_name,alias))
+	return alias
+
 class SqlGraph(object):
 	"""SqlGraph generates a Directed Graph from Sql joins that are implied by column names and AutoTest profiles"""
 	def __init__(self, server, user, password, database, query_file):
@@ -95,11 +113,42 @@ class SqlGraph(object):
 			print('\nSELECT *\nFROM {}'.format(node))
 		for edge in self.DiG.out_edges(node):
 			# print('\tedge: {}->{} {}'.format(*edge,self.DiG.get_edge_data(*edge)))
-			colNames.append(self.DiG.get_edge_data(*edge)['Column'])
-			print('{}JOIN {}\n{}ON {}.{}={}.{}'.format('\t'*i,edge[1],'\t'*i,edge[1],self.DiG.get_edge_data(*edge)['Column'],edge[0],self.DiG.get_edge_data(*edge)['Column']))
-			sql.append('{}JOIN {}\n{}ON {}.{}={}.{}'.format('\t'*i,edge[1],'\t'*i,edge[1],self.DiG.get_edge_data(*edge)['Column'],edge[0],self.DiG.get_edge_data(*edge)['Column']))
+			colNames.append('{}.{}'.format(edge[0],self.DiG.get_edge_data(*edge)['Column']))
+			print('{}LEFT JOIN {}\n{}ON {}.{}={}.{}'.format('\t'*i,edge[1],'\t'*i,edge[1],self.DiG.get_edge_data(*edge)['Column'],edge[0],self.DiG.get_edge_data(*edge)['Column']))
+			sql.append('{}LEFT JOIN {}\n{}ON {}.{}={}.{}'.format('\t'*i,edge[1],'\t'*i,edge[1],self.DiG.get_edge_data(*edge)['Column'],edge[0],self.DiG.get_edge_data(*edge)['Column']))
 			self._generate_sql_parts(edge[1],i+1,colNames,sql)
-		return colNames,sql
+		# remove colNames of root
+		good_colNames = []
+		for colName in colNames:
+			if node not in colName:
+				good_colNames.append(colName)
+		return good_colNames,sql
+
+	def _generate_subgraph_sql_parts(self, node, subgraph, i=0,colNames=None,sql=None):
+		""" 
+			generates sql string parts from self.DiG (assumes self.DiG already exists) that are combined into useable string in self.get_sql(table_name) 
+			called by self.get_sql(table_name)
+			utility function that traverses up DiGraph object generating joins and column name list 
+
+			TODO: node is redundant and should be inferred from subgraph
+		"""
+		if i == 0:
+			sql=[]
+			colNames=[]
+			print('\nSELECT *\nFROM {}'.format(node))
+		for edge in subgraph.out_edges(node):
+			# print('\tedge: {}->{} {}'.format(*edge,subgraph.get_edge_data(*edge)))
+			colNames.append('{}.{}'.format(edge[0],subgraph.get_edge_data(*edge)['Column']))
+			print('{}LEFT JOIN {}\n{}ON {}.{}={}.{}'.format('\t'*i,edge[1],'\t'*i,edge[1],subgraph.get_edge_data(*edge)['Column'],edge[0],subgraph.get_edge_data(*edge)['Column']))
+			sql.append('{}LEFT JOIN {}\n{}ON {}.{}={}.{}'.format('\t'*i,edge[1],'\t'*i,edge[1],subgraph.get_edge_data(*edge)['Column'],edge[0],subgraph.get_edge_data(*edge)['Column']))
+			self._generate_subgraph_sql_parts(edge[1],subgraph,i+1,colNames,sql)
+		# remove colNames of root
+		good_colNames = []
+		for colName in colNames:
+			if node not in colName:
+				good_colNames.append(colName)
+		return good_colNames,sql
+
 
 	def get_sql(self, table_name):
 		"""
@@ -109,17 +158,29 @@ class SqlGraph(object):
 		query='SELECT\n\t{}\nFROM {}\n{}'.format('\n\t,'.join(colNames),table_name,'\n'.join(sql))
 		return query
 
+	def get_sql2(self, table_name):
+		pass
 	def get_subgraph(self,node,subgraph=None):
 		"""
 			recurses up self.DiG from given node (ie table name)  building/populating a subgraph object
 		"""
 		if subgraph == None:
-			subgraph = nx.DiGraph(database=database, server=server, root=node)
+			subgraph = nx.DiGraph(database=self.database, server=self.server, root=node)
 		for edge in self.DiG.out_edges(node):
+			# subgraph.add_node(edge[1],{'alias':get_alias(edge[1])})
+			# if edge[0] in subgraph:
+			# 	print('child node: {} already in subgraph'.format(edge[0]))
+			# if edge[1] in subgraph:
+			# 	print('parent node: {} already in subgraph'.format(edge[1]))
+			# get_alias(edge[0])
 			subgraph.add_edge(node, edge[1],{'Column':self.DiG.get_edge_data(*edge)['Column']})
 			self.get_subgraph(edge[1],subgraph)
+		# for node in subgraph:
+		# 	subgraph[node]['alias'] = get_alias(node)
 		return subgraph
 
+
+	### short cut functions
 	def plot_sql(self, table_name=None):
 		"""
 			use matplotlib plotting module to show visual of graph
@@ -134,8 +195,6 @@ class SqlGraph(object):
 		nx.draw_spring(self.DiG)
 		plt.draw()
 		plt.show()
-
-	### short cut functions
 	def save_BizRule(self,table_name=None):
 		if table_name == None:
 			for n in self.DiG:
@@ -143,15 +202,25 @@ class SqlGraph(object):
 				br = BizRule()
 				br.short_name = n
 				br.action_sql = sql
-				with open('{}.sql'.format(n),'w') as fout:
+				with open('BizRule_{}.sql'.format(n),'w') as fout:
 					fout.write(str(br))
 		else:
 			sql = self.get_sql(table_name)
 			br = BizRule()
 			br.short_name = table_name
 			br.action_sql = sql
-			with open('{}.sql'.format(table_name),'w') as fout:
+			with open('BizRule_{}.sql'.format(table_name),'w') as fout:
 				fout.write(str(br))
+	def save_sql(self, table_name=None):
+		if table_name == None:
+			for n in self.DiG:
+				sql = self.get_sql(n)
+				with open('{}.sql'.format(n),'w') as fout:
+					fout.write(str(sql))
+		else:
+			sql = self.get_sql(table_name)
+			with open('{}.sql'.format(table_name),'w') as fout:
+				fout.write(str(sql))
 	def test_sql(self, table_name):
 		print('connecting to Sql Server:\n\tserver: {server}\n\tdatabase: {database}'.format(**self.__dict__))
 		with pymssql.connect(self.server, self.user, self.password, self.database) as conn:
@@ -168,5 +237,6 @@ if __name__ == '__main__':
 	
 	# sql_graph.save_BizRule()
 	table_name='dbo.ReferralFact'
-	# sql_graph.get_sql(table_name)
-	sql_graph.test_sql(table_name)
+	sql_graph.save_sql(table_name)
+	subgraph=sql_graph.get_subgraph(table_name)
+	sql_graph._generate_subgraph_sql_parts(table_name,subgraph)
