a9551c943842f6f400d92149f06f3fc770cf5d03
grahamcrowell_vch <graham.crowell@gmail.com>
2017-02-22 19:06:17 -0800

    AutoTestReport

---
+++ AutoTestReport/AutoTestConfigProcs.sql
@@ -0,0 +1,24 @@
+﻿
+--CREATE TABLE AutoTest.dbo.tmp 
+--(
+--DatabaseName varchar(500) DEFAULT NULL
+--);
+--GO
+--CREATE PROC dbo.TurnOffDatabase @databaseName varchar(400)
+--AS
+--BEGIN
+--	IF @databaseName IS NOT NULL
+--	BEGIN
+	
+--		INSERT INTO AutoTest.dbo.tmp VALUES(@databaseName);
+
+--	END
+--END
+
+--GO
+
+
+--DELETE AutoTest.dbo.tmp 
+
+SELECT *
+FROM AutoTest.dbo.tmp 
